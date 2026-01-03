9b9c625ecbd0b876fc8e2b1faaf8a4744f265609
Léo Cazenave <70967142+Nuclear-Squid@users.noreply.github.com>
2026-01-03 21:39:24 +0000

    Common `keymap` folder (#5)

--- flake.nix
+++ flake.nix
@@ -2,28 +2,35 @@
   description = "Development flake for the Quacken’s ZMK firmware";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    self-pkgs = self.packages.x86_64-linux;
+  in {
+    packages.x86_64-linux.python = (pkgs.python3.withPackages (
+      pypkgs: with pypkgs; [
+        west
+        pyelftools
+        pyyaml
+        pykwalify
+        canopen
+        packaging
+        progress
+        psutil
+        pylink-square
+        pyserial
+        requests
+        anytree
+        intelhex
+      ]
+    ));
+
     devShells.x86_64-linux.default = pkgs.mkShell {
       buildInputs = with pkgs; [
-        (python3.withPackages (py-pkgs: with py-pkgs; [
-            pyelftools
-            pyyaml
-            pykwalify
-            canopen
-            packaging
-            progress
-            psutil
-            pylink-square
-            pyserial
-            requests
-            anytree
-            intelhex
-            west
-        ]))
-
+        self-pkgs.python
+        cmake
         ninja
         dfu-util
+        minicom
       ];
     };
   };

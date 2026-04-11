a09f39bc09a394d068e510a3ecda8f37eb0e7f83
Léo Cazenave <70967142+Nuclear-Squid@users.noreply.github.com>
2026-04-11 15:46:22 +0000

    Update src/behaviors/behavior_dead_key.c
    Co-authored-by: Fabien Cazenave <fabi1cazenave@users.noreply.github.com>

--- src/behaviors/behavior_dead_key.c
+++ src/behaviors/behavior_dead_key.c
@@ -3,6 +3,7 @@
  * Press a non-modified dead key before the target key.
  * This ensures the dead key is always applied without any modifier, even if
  * Shift or Ctrl or Cmd are pressed while the target key is tapped.
+ */
 
 #define DT_DRV_COMPAT zmk_behavior_dead_key
 
